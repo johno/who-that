d691f5c6d5f41f1e918eb665733225a58a9cabfa
John Otander <johnotander@gmail.com>
2016-08-18 15:53:04 -0600

    Retrieve pinned repos

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict'
 
 const ghUser = require('gh-user')
+const pinnedRepos = require('gh-pinned-repos')
 const npmUser = require('npm-user')
 
 module.exports = function whoThat (who) {
   return new Promise((resolve, reject) => {
     ghUser(who.github)
       .then(gh => Object.assign({}, gh))
+      .then(data => (
+        pinnedRepos(who.github)
+          .then(pinned => Object.assign({}, data, { pinned }))
+      ))
       .then(data => {
         if (who.npm) {
           return npmUser(who.npm).then(npm => Object.assign({}, npm, data))
