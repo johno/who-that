c47abc8d95efb90fa2ac6e8ef134a08d62d85617
John Otander <johnotander@gmail.com>
2016-12-04 16:32:26 -0700

    Use const where applicable

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ const githubUsername = require('github-username')
 
 module.exports = function whoThat (who) {
   return new Promise((resolve, reject) => {
-    let findWho = Object.assign({}, who)
-    let firstFind = (isBlank(findWho.github)) ? githubUsername(findWho.email) : ghUser(findWho.github)
+    const findWho = Object.assign({}, who)
+    const firstFind = isBlank(findWho.github) ? githubUsername(findWho.email) : ghUser(findWho.github)
+
     firstFind
       .then(data => {
         if (isBlank(findWho.github)) {
           findWho.github = data
           return ghUser(data).then(gh => Object.assign({}, gh))
         }
+
         return Object.assign({}, data)
       })
       .then(data => pinnedRepos(findWho.github).then(pinned => Object.assign({}, data, { pinned })))
       .then(data => {
         if (isBlank(findWho.npm)) return data
+
         return npmUser(findWho.npm)
           .then(npm => Object.assign({}, npm, data))
           .then(data => pkgList(findWho.npm).then(pkgs => Object.assign({}, { pkgs }, data)))
